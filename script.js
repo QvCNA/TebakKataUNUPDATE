ef3f1ec22e5ff28d2e587f7ad85364602bdb6661
Fazrel <161111358+QvCNA@users.noreply.github.com>
2024-02-26 20:56:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const wordText = document.querySelector(".word"),
+    hintText = document.querySelector(".hint span"),
+    timeText = document.querySelector(".time b"),
+    inputField = document.querySelector("input"),
+    refreshBtn = document.querySelector(".refresh-word"),
+    checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            return (timeText.innerText = maxTime);
+        }
+        alert(`Waktu Habis! ${correctWord.toUpperCase()} Selamat Kamu Benar :)`);
+        initGame();
+    }, 1000);
+};
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+};
+initGame();
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if (!userWord) return alert("Isi Kata!");
+    if (userWord !== correctWord)
+        return alert(`Oops! ${userWord} Nice Try Sayang,Coba Lagi :)`);
+    alert(`Yaps! ${correctWord.toUpperCase()} Kamu Benar :)`);
+    initGame();
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "faisal",
+        hint: "yang sering dibully"
+    },
+    {
+        word: "fazrel",
+        hint: "pembuat game ini"
+    },
+    {
+        word: "novia",
+        hint: "acil-acil jual bakwan"
+    },
+    {
+        word: "zaini",
+        hint: "nama bapak reza"
+    },
+    {
+        word: "faiz",
+        hint: "yang sering bilang yatim"
+    },
+    {
+        word: "mustafa",
+        hint: "si paling ff"
+    },
+    {
+        word: "afka alvarez",
+        hint: "anak meme"
+    },
+    {
+        word: "jepri",
+        hint: "si paling osis"
+    },
+    {
+        word: "nispi",
+        hint: "novia"
+    },
+    {
+        word: "zaini",
+        hint: "penyambatan"
+    },
+    {
+        word: "ridho",
+        hint: "orangnya alim"
+    },
+    {
+        word: "rifky",
+        hint: "cebol selain opal"
+    },
+    {
+        word: "arsyad",
+        hint: "landak kalimantan"
+    },
+    {
+        word: "matematika",
+        hint: "pelajaran yang di benci umat manusia"
+    },
+    {
+        word: "olahraga",
+        hint: "yang di tunggu laki-laki pada hari kamis"
+    },
+    {
+        word: "kantin",
+        hint: "jam kos pada kemana?"
+    },
+    {
+        word: "naufal",
+        hint: "kecil tapi seperti raja"
+    },
+    {
+        word: "abdurrahman",
+        hint: "tinggi,badak,rambut belah dua"
+    },
+    {
+        word: "faisal",
+        hint: "yang paling cool"
+    },
+    {
+        word: "dadang",
+        hint: "biasalah faisal..."
+    },
+    {
+        word: "goyang nasi padang",
+        hint: "yang di nyanyikan amang madi"
+    },
+    {
+        word: "perpus",
+        hint: "gada kerjaan"
+    },
+    {
+        word: "gibah",
+        hint: "kebiasaan cewek?"
+    }
+];
